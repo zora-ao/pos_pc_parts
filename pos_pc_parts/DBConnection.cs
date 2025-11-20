49b53abad00fd8c58c6d989a7c9c5faa4bf174a0
Carl Jasper Ramos <zoraao79@gmail.com>
2025-11-20 21:13:23 +0800

    Add project files.

---
+++ pos_pc_parts/DBConnection.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pos_pc_parts
+{
+    internal class DBConnection
+    {
+        public string GetConnection()
+        {
+            return "server=localhost;database=pc_parts_pos;uid=root;pwd=A40o8ccjr";
+        }
+    }
+}
---
+++ pos_pc_parts/Form1.Designer.cs
@@ -0,0 +1,381 @@
+﻿namespace pos_pc_parts
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel1 = new Panel();
+            pictureBox2 = new PictureBox();
+            panel2 = new Panel();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            button3 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            dashboard_main_panel = new Panel();
+            panel6 = new Panel();
+            dataGridView1 = new DataGridView();
+            colId = new DataGridViewTextBoxColumn();
+            colName = new DataGridViewTextBoxColumn();
+            colCategory = new DataGridViewTextBoxColumn();
+            colPrice = new DataGridViewTextBoxColumn();
+            colQuantity = new DataGridViewTextBoxColumn();
+            colEdit = new DataGridViewImageColumn();
+            colDelete = new DataGridViewImageColumn();
+            panel3 = new Panel();
+            panel4 = new Panel();
+            lbTotalItems = new Label();
+            label2 = new Label();
+            panel5 = new Panel();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            dashboard_main_panel.SuspendLayout();
+            panel6.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
+            panel5.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.CornflowerBlue;
+            panel1.Controls.Add(pictureBox2);
+            panel1.Dock = DockStyle.Top;
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(900, 45);
+            panel1.TabIndex = 0;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = Properties.Resources.EZPC;
+            pictureBox2.Location = new Point(37, 12);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(74, 21);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.CornflowerBlue;
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(button3);
+            panel2.Controls.Add(button4);
+            panel2.Controls.Add(button2);
+            panel2.Controls.Add(button1);
+            panel2.Dock = DockStyle.Left;
+            panel2.Location = new Point(0, 45);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(148, 505);
+            panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(37, 102);
+            label1.Name = "label1";
+            label1.Size = new Size(61, 21);
+            label1.TabIndex = 2;
+            label1.Text = "Admin";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, 33);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(102, 66);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(21, 221);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 38);
+            button3.TabIndex = 0;
+            button3.Text = "Add Products";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(21, 440);
+            button4.Name = "button4";
+            button4.Size = new Size(102, 38);
+            button4.TabIndex = 0;
+            button4.Text = "Logout";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(21, 284);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 38);
+            button2.TabIndex = 0;
+            button2.Text = "Stock";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(21, 165);
+            button1.Name = "button1";
+            button1.Size = new Size(102, 38);
+            button1.TabIndex = 0;
+            button1.Text = "Dashboard";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // dashboard_main_panel
+            // 
+            dashboard_main_panel.Controls.Add(panel6);
+            dashboard_main_panel.Controls.Add(panel3);
+            dashboard_main_panel.Dock = DockStyle.Fill;
+            dashboard_main_panel.Location = new Point(148, 45);
+            dashboard_main_panel.Name = "dashboard_main_panel";
+            dashboard_main_panel.Size = new Size(752, 505);
+            dashboard_main_panel.TabIndex = 2;
+            // 
+            // panel6
+            // 
+            panel6.Controls.Add(dataGridView1);
+            panel6.Dock = DockStyle.Fill;
+            panel6.Location = new Point(0, 154);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(752, 351);
+            panel6.TabIndex = 3;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { colId, colName, colCategory, colPrice, colQuantity, colEdit, colDelete });
+            dataGridView1.Dock = DockStyle.Fill;
+            dataGridView1.Location = new Point(0, 0);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.ReadOnly = true;
+            dataGridView1.Size = new Size(752, 351);
+            dataGridView1.TabIndex = 0;
+            // 
+            // colId
+            // 
+            colId.HeaderText = "Id";
+            colId.Name = "colId";
+            colId.ReadOnly = true;
+            colId.Visible = false;
+            // 
+            // colName
+            // 
+            colName.AutoSizeMode = DataGridViewAutoSizeColumnMode.Fill;
+            colName.HeaderText = "Name";
+            colName.Name = "colName";
+            colName.ReadOnly = true;
+            // 
+            // colCategory
+            // 
+            colCategory.HeaderText = "Category";
+            colCategory.Name = "colCategory";
+            colCategory.ReadOnly = true;
+            // 
+            // colPrice
+            // 
+            colPrice.HeaderText = "Price";
+            colPrice.Name = "colPrice";
+            colPrice.ReadOnly = true;
+            // 
+            // colQuantity
+            // 
+            colQuantity.HeaderText = "Quantity";
+            colQuantity.Name = "colQuantity";
+            colQuantity.ReadOnly = true;
+            // 
+            // colEdit
+            // 
+            colEdit.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            colEdit.HeaderText = "Edit";
+            colEdit.Image = Properties.Resources.edit;
+            colEdit.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            colEdit.Name = "colEdit";
+            colEdit.ReadOnly = true;
+            colEdit.Width = 33;
+            // 
+            // colDelete
+            // 
+            colDelete.AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCells;
+            colDelete.HeaderText = "Delete";
+            colDelete.Image = Properties.Resources.trash_2;
+            colDelete.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            colDelete.Name = "colDelete";
+            colDelete.ReadOnly = true;
+            colDelete.Width = 46;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(panel4);
+            panel3.Controls.Add(panel5);
+            panel3.Dock = DockStyle.Top;
+            panel3.Location = new Point(0, 0);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(752, 154);
+            panel3.TabIndex = 2;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.CornflowerBlue;
+            panel4.Controls.Add(lbTotalItems);
+            panel4.Controls.Add(label5);
+            panel4.Location = new Point(77, 33);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(200, 100);
+            panel4.TabIndex = 0;
+            // 
+            // lbTotalItems
+            // 
+            lbTotalItems.AutoSize = true;
+            lbTotalItems.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbTotalItems.Location = new Point(73, 44);
+            lbTotalItems.Name = "lbTotalItems";
+            lbTotalItems.Size = new Size(37, 29);
+            lbTotalItems.TabIndex = 0;
+            lbTotalItems.Text = "25";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(64, 17);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 15);
+            label2.TabIndex = 0;
+            label2.Text = "Total Sales:";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.CornflowerBlue;
+            panel5.Controls.Add(label3);
+            panel5.Controls.Add(label2);
+            panel5.Controls.Add(label4);
+            panel5.Location = new Point(424, 33);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(200, 100);
+            panel5.TabIndex = 1;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.Location = new Point(64, 44);
+            label3.Name = "label3";
+            label3.Size = new Size(61, 29);
+            label3.TabIndex = 0;
+            label3.Text = "5000";
+            // 
+            // label4
+            // 
+            label4.Location = new Point(0, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(100, 23);
+            label4.TabIndex = 1;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.Location = new Point(25, 17);
+            label5.Name = "label5";
+            label5.Size = new Size(153, 15);
+            label5.TabIndex = 0;
+            label5.Text = "Total Number of Products:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(900, 550);
+            Controls.Add(dashboard_main_panel);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "Form1";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            dashboard_main_panel.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Button button3;
+        private Button button4;
+        private Button button2;
+        private Button button1;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Panel dashboard_main_panel;
+        private Panel panel5;
+        private Panel panel4;
+        private Label label2;
+        private Label label4;
+        private Label label3;
+        private Label lbTotalItems;
+        private Panel panel6;
+        private DataGridView dataGridView1;
+        private Panel panel3;
+        private DataGridViewTextBoxColumn colId;
+        private DataGridViewTextBoxColumn colName;
+        private DataGridViewTextBoxColumn colCategory;
+        private DataGridViewTextBoxColumn colPrice;
+        private DataGridViewTextBoxColumn colQuantity;
+        private DataGridViewImageColumn colEdit;
+        private DataGridViewImageColumn colDelete;
+        private Label label5;
+    }
+}
